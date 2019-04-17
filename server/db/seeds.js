bd21a5bf4e5f8687371a7f07fbb4d667fa1735ec
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-04-17 10:27:26 +0100

    seed file

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,1 +1,19 @@
-//placeholder
+use bucketlist;
+db.dropDatabase();
+
+db.items.insertMany([{
+    title: "Complete CodeClan Course",
+    date: "2019-04",
+    deadline: "2019-06",
+    completed: false
+},{
+    title: "Go freediving",
+    date: "2019-04",
+    deadline: "2020-06",
+    completed: false
+},{
+    title: "Bag Sgurr Alasdair",
+    date: "1996-01",
+    deadline: "2019-07",
+    completed: false
+}])
