0130f2100ca6d2b9547c83e028c163821cfe0efa
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-04-17 14:21:58 +0100

    commit

--- client/src/views/form_view.js
+++ client/src/views/form_view.js
@@ -8,4 +8,27 @@ FormView.prototype.bindEvents = function(){
 
 };
 
+SightingFormView.prototype.bindEvents = function () {
+  this.form.addEventListener('submit', (evt) => {
+    this.handleSubmit(evt);
+  });
+};
+
+SightingFormView.prototype.handleSubmit = function (evt) {
+  evt.preventDefault();
+  const newSighting = this.createSighting(evt.target);
+  PubSub.publish('SightingFormView:sighting-submitted', newSighting);
+  evt.target.reset();
+};
+
+SightingFormView.prototype.createSighting = function (form) {
+  const newSighting = {
+    species: form.species.value,
+    location: form.location.value,
+    date: form.date.value
+  };
+
+  return newSighting;
+};
+
 module.exports = FormView;
