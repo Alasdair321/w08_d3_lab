9c2f27fa32d566ac6191cbb6384243cf3c9df5ed
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-04-17 10:37:27 +0100

    create_router index

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,1 +1,17 @@
-//placeholder
+const express = require('express');
+const ObjectID = require('mongoDB').ObjectID;
+
+const createRouter = function (collection){
+    const router = express.Router();
+
+    // index
+    router.get('/', (req, res)=>{
+        collection.find().toArray()
+        .then((docs)=>{res.json(docs)})
+        .catch((err)=>{
+            res.status(500);
+            res.json({status: 500, error: err});
+        });
+    });
+
+}
