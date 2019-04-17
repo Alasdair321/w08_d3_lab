b1a66a677d2d2614d02735e1e25684c53f8a7be1
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-04-17 11:53:31 +0100

    create_router is done

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -53,6 +53,20 @@ const createRouter = function (collection){
     })
 
     // update
+
+    router.put('/:id', (req, res) => {
+        const id = req.params.id;
+        const updatedData = req.body;
+        collection
+        .updateOne({_id: ObjectID(id)},{$set: updatedData})
+        .then(() => collection.find().toArray())
+        .then(docs => res.json(docs))
+        .catch((err)=>{
+            res.status(500);
+            res.json({status: 500, error: err});
+        });
+    })
+
     return router;
 };
 
