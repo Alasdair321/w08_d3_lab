15943df54600f2971fad9e1febab5047a8a5951c
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-04-17 13:34:54 +0100

    bucket-list.js 60% done

--- client/src/models/bucket_list.js
+++ client/src/models/bucket_list.js
@@ -7,7 +7,79 @@ const BucketList = function(url){
 };
 
 BucketList.prototype.bindEvents = function(){
+  PubSub.subscribe('FormView:Submitted', (evt)=>{
+    this.postItem(evt.detail); // create
+  });
+  PubSub.subscribe('ListView:deleteitem', (evt)=>{
+    this.deleteItem(evt.detail);
+  });
+  PubSub.subscribe('ListView:edititem', (evt)=>{
+    this.editItem(evt.detail);
+  });
+  PubSub.subscribe('listView:completeitem', (evt)=>{
+    this.completeItem(evt.detail);
+  })
+};
 
+BucketList.prototype.getData = function () {
+  this.request.get()
+    .then((bucketlist) => {
+      PubSub.publish('Bucketlist:list-data', bucketlist);
+    })
+    .catch(console.error);
+};
+
+BucketList.prototype.postItem = function (item) {
+  this.request.post(item)
+    .then((items) => {
+      PubSub.publish('Bucketlist:list-data', items);
+    })
+    .catch(console.error);
+};
+
+BucketList.prototype.deleteItem = function (itemID) {
+  this.request.delete(itemID)
+    .then((items) => {
+      PubSub.publish('Bucketlist:list-data', items);
+    })
+    .catch(console.error);
 };
 
 module.exports = BucketList;
+
+
+// Sightings.prototype.bindEvents = function () {
+//   PubSub.subscribe('SightingView:sighting-delete-clicked', (evt) => {
+//     this.deleteSighting(evt.detail);
+//   });
+
+//   PubSub.subscribe('SightingFormView:sighting-submitted', (evt) => {
+//     this.postSighting(evt.detail);
+//   })
+// };
+
+// Sightings.prototype.getData = function () {
+//   this.request.get()
+//     .then((sightings) => {
+//       PubSub.publish('Sightings:data-loaded', sightings);
+//     })
+//     .catch(console.error);
+// };
+
+// Sightings.prototype.postSighting = function (sighting) {
+//   this.request.post(sighting)
+//     .then((sightings) => {
+//       PubSub.publish('Sightings:data-loaded', sightings);
+//     })
+//     .catch(console.error);
+// };
+
+// Sightings.prototype.deleteSighting = function (sightingId) {
+//   this.request.delete(sightingId)
+//     .then((sightings) => {
+//       PubSub.publish('Sightings:data-loaded', sightings);
+//     })
+//     .catch(console.error);
+// };
+
+// module.exports = Sightings;
