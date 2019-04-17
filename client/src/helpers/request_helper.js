b77d55fd302ec64133542f27296f21e68a6a1c8d
ericxcross <eric.cross@sympatico.ca>
2019-04-17 09:52:00 +0100

    file structure created

---
+++ client/src/helpers/request_helper.js
@@ -0,0 +1,33 @@
+const RequestHelper = function (url) {
+  this.url = url;
+};
+
+//INDEX
+RequestHelper.prototype.get = function () {
+  return fetch(this.url)
+    .then((response) => response.json());
+};
+
+//DELETE
+RequestHelper.prototype.delete = function (id) {
+  return fetch(`${this.url}/${id}`, {
+    method: 'DELETE'
+  })
+    .then((response) => response.json());
+};
+
+//CREATE
+RequestHelper.prototype.post = function (payload) {
+  return fetch(`${this.url}/`, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((response) => response.json());
+};
+
+//UPDATE
+
+//GET
+
+module.exports = RequestHelper;
